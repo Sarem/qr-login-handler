f12ffc64b47cc7e42349761b96b5e9d08ac2898a
@sarem <gent.sami@gmail.com>
2020-10-13 13:18:48 +0330

    docker-compose.yml bugfix

--- src/main/java/ir/aio/qr/qrloginhandler/LoginController.java
+++ src/main/java/ir/aio/qr/qrloginhandler/LoginController.java
@@ -2,26 +2,25 @@ package ir.aio.qr.qrloginhandler;
 
 import lombok.AllArgsConstructor;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("qr")
 @AllArgsConstructor
+@ControllerAdvice
 public class LoginController {
 
     QrService qrService;
     private final ReactiveRedisOperations<String, QrSession> qrSessionOps;
 
     @GetMapping("listen/{qr}")
-    public Mono<QrSession> listen(@PathVariable String qr){
+    public Mono<QrSession> listen(@PathVariable String qr) {
         return qrService.pop(qr);
     }
 
     @PutMapping("put")
-    public Mono<Long> put(@RequestBody /*MultiValueMap<String, String> formData*/ QrSession qrSession){
+    public Mono<Long> put(@RequestBody QrSession qrSession) {
         return qrService.put(qrSession);
     }
 
--- src/main/java/ir/aio/qr/qrloginhandler/QrService.java
+++ src/main/java/ir/aio/qr/qrloginhandler/QrService.java
@@ -1,30 +1,31 @@
 package ir.aio.qr.qrloginhandler;
 
 import lombok.AllArgsConstructor;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-import javax.annotation.PostConstruct;
 import java.time.Duration;
 import java.util.UUID;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class QrService {
 
-    private final ReactiveRedisConnectionFactory factory;
     private final ReactiveRedisOperations<String, QrSession> qrSessionOps;
 
 
     public Mono<String> generateQR() {
         return Mono.fromSupplier(() -> UUID.randomUUID().toString());
     }
-    public Mono<Long> put(QrSession qrSession){
-        return qrSessionOps.opsForList().leftPush(qrSession.getId(),qrSession);
+
+    public Mono<Long> put(QrSession qrSession) {
+        return qrSessionOps.opsForList().leftPush(qrSession.getId(), qrSession);
     }
-    public Mono<QrSession> pop(String s){
+
+    public Mono<QrSession> pop(String s) {
         return qrSessionOps.opsForList().leftPop(s, Duration.ofSeconds(100));
     }
 }
